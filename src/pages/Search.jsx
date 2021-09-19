4ca1b15a41355e629bb4b1bf0f85ffdad1f6c020
Victor Varges <victorryandias@gmail.com>
2021-09-19 20:14:52 -0300

    criando formulario em search para pesquisar por bandas e musicas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,47 @@
 import React from 'react';
 import Header from './componentes/Header';
 
+const MIN_CARACTERES = 2;
+
 export default class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      pesquisa: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      pesquisa: event.target.value,
+    });
+  }
+
   render() {
+    const { pesquisa } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>Teste</p>
+        <form>
+          <input
+            type="text"
+            value={ pesquisa }
+            name="pesquisa"
+            data-testid="search-artist-input"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            name="button"
+            data-testid="search-artist-button"
+            disabled={ pesquisa.length < MIN_CARACTERES }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
