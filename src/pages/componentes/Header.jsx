b9516dae67ccc5d87ad4f8926c2428da70a8882c
Victor Varges <victorryandias@gmail.com>
2021-09-19 19:28:16 -0300

    criando os links para o meu Header

--- src/pages/componentes/Header.jsx
+++ src/pages/componentes/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../../services/userAPI';
 import Loading from './Loading';
 
@@ -29,7 +30,25 @@ export default class Header extends React.Component {
     return (
       <header data-testid="header-component">
         {loading ? <Loading /> : null}
-        <h2 data-testid="header-user-name">{ user }</h2>
+        <h2 data-testid="header-user-name">{user}</h2>
+        <Link
+          to="/search"
+          data-testid="link-to-search"
+        >
+          Search
+        </Link>
+        <Link
+          to="/favorites"
+          data-testid="link-to-favorites"
+        >
+          Favorites
+        </Link>
+        <Link
+          to="/profile"
+          data-testid="link-to-profile"
+        >
+          Profile
+        </Link>
       </header>
     );
   }
