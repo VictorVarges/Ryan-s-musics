58fd39496704694545aa54f9d7fc9576e382c3f3
Victor Varges <victorryandias@gmail.com>
2021-09-20 23:52:49 -0300

    renderizando albums atraves do map, condicionais criadas e criando link nos proprios albums (requisito 6)

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from './componentes/Header';
+import Loading from './componentes/Loading';
 
 const MIN_CARACTERES = 2;
 
@@ -8,9 +11,18 @@ export default class Search extends React.Component {
     super();
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleForm = this.handleForm.bind(this);
+    this.handleAPI = this.handleAPI.bind(this);
+    this.renderAlbumsList = this.renderAlbumsList.bind(this);
 
     this.state = {
       pesquisa: '',
+      albums: [],
+      showForm: true,
+      loading: false,
+      artist: '',
+      apiOk: false,
     };
   }
 
@@ -20,28 +32,111 @@ export default class Search extends React.Component {
     });
   }
 
-  render() {
+  async handleClick() {
     const { pesquisa } = this.state;
+    // antes da API.
+    this.setState({
+      showForm: false,
+      loading: true,
+      apiOk: false,
+    });
+    const searchAlbum = await searchAlbumsAPI(pesquisa);
+
+    let artistOurBand = pesquisa;
+    if (searchAlbum.length === 0) {
+      artistOurBand = '';
+    }
+    // Após a API ser invocada.
+    this.setState({
+      pesquisa: '',
+      showForm: true,
+      albums: searchAlbum,
+      loading: false,
+      artist: artistOurBand,
+      apiOk: true,
+    });
+  }
+
+  handleForm() {
+    const { pesquisa } = this.state;
+    return (
+      <form>
+        <input
+          type="text"
+          value={ pesquisa }
+          name="pesquisa"
+          data-testid="search-artist-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          name="buttonSearch"
+          data-testid="search-artist-button"
+          disabled={ pesquisa.length < MIN_CARACTERES }
+          onClick={ this.handleClick }
+        >
+          Pesquisar
+        </button>
+      </form>
+    );
+  }
+
+  handleAPI() {
+    const { artist } = this.state;
+    const { apiOk } = this.state;
+    if (apiOk === true) {
+      if (artist !== '') {
+        return (
+          <p>
+            {`Resultado de álbuns de: ${artist}`}
+          </p>
+        );
+      }
+      return (
+        <p>
+          Nenhum álbum foi encontrado
+        </p>
+      );
+    }
+  }
+
+  renderAlbumsList() {
+    const { albums } = this.state;
+    return (
+      <ul>
+        {albums.map((album) => (
+          <li key={ album.collectionId }>
+            <Link
+              data-testid={ `link-to-album-${album.collectionId}` }
+              to={ `/album/${album.collectionId}` }
+            >
+              <img
+                src={ album.artworkUrl100 }
+                alt={ album.collectionName }
+              />
+            </Link>
+            <h3>
+              {album.collectionName}
+            </h3>
+            <h5>
+              {album.artistName}
+            </h5>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  render() {
+    const { showForm } = this.state;
+    const { loading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <input
-            type="text"
-            value={ pesquisa }
-            name="pesquisa"
-            data-testid="search-artist-input"
-            onChange={ this.handleChange }
-          />
-          <button
-            type="button"
-            name="button"
-            data-testid="search-artist-button"
-            disabled={ pesquisa.length < MIN_CARACTERES }
-          >
-            Pesquisar
-          </button>
-        </form>
+        {showForm ? this.handleForm() : null}
+        {loading ? <Loading /> : null}
+        {this.handleAPI()}
+        {this.renderAlbumsList()}
       </div>
     );
   }
