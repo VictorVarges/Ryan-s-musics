4e9e171c6581ee314758b571d78c52c26c1a8dad
Victor Varges <victorryandias@gmail.com>
2021-10-11 22:21:14 -0300

    requirement 11, done!

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './componentes/Header';
+// import Loading from '../components/Loading';
+// import MusicCard from '../components/MusicCard';
+// import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends React.Component {
   render() {
     return (
-      <div data-testid="page-favorites">
+      <div
+        data-testid="page-favorites"
+      >
         <Header />
         <p>Teste</p>
       </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './componentes/Header';
+import { getUser } from '../services/userAPI';
+import Loading from './componentes/Loading';
 
 export default class Profile extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      user: [],
+    };
+    this.infoUser = this.infoUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.infoUser();
+  }
+
+  async infoUser() {
+    const objectUser = await getUser();
+    this.setState({
+      user: objectUser,
+      loading: false });
+  }
+
+  // utilizei este repositório como referência: https://github.com/tryber/sd-014-a-project-trybetunes/pull/104/files
+  renderProfile(user) {
+    return (
+      <div>
+        <h1>{user.name}</h1>
+        <h3>{user.email}</h3>
+        <img data-testid="profile-image" src={ user.image } alt={ user.name } />
+        <p>{user.description}</p>
+        <Link to="/profile/edit">
+          <h4>Editar perfil</h4>
+        </Link>
+      </div>
+    );
+  }
+
   render() {
+    const { loading, user } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        <p>Teste</p>
+        { loading ? <Loading /> : this.renderProfile(user) }
       </div>
     );
   }
